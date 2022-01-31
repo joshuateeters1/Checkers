6af94fb19ada44d018778cf09d91d8778af782f7
Joshua Teeters <joshuateeters1@gmail.com>
2022-01-30 23:24:04 -0600

    changed board constants to class constants

--- board.py
+++ board.py
@@ -1,21 +1,22 @@
-NUM_ROWS = 8
-NUM_COLUMNS = 8
-RED = 'r'
-BLACK = 'b'
-RED_KING = 'R'
-BLACK_KING = 'B'
-EMPTY = '-'
-OOB = '.'
-letterToNumber = {'a': 0,'b': 1,'c': 2,'d': 3,'e': 4,'f': 5,'g': 6,'h': 7}
 
 class Board:
+    NUM_ROWS = 8
+    NUM_COLUMNS = 8
+    RED = 'r'
+    BLACK = 'b'
+    RED_KING = 'R'
+    BLACK_KING = 'B'
+    EMPTY = '-'
+    OOB = '.'
+    letterToNumber = {'a': 0,'b': 1,'c': 2,'d': 3,'e': 4,'f': 5,'g': 6,'h': 7}
+
     board = []
 
     def __init__(self):
         self.resetBoard()
 
     def conv(self, col):
-        return letterToNumber[col]
+        return self.letterToNumber[col]
 
     def set(self, col, row, value):
         self.board[col][row] = value
@@ -38,51 +39,52 @@ class Board:
 
     def clearBoard(self):
         self.board = []
-        for i in range(NUM_COLUMNS):
+        for i in range(self.NUM_COLUMNS):
             col = []
-            for j in range(NUM_ROWS):
-                col.append(EMPTY)
+            for j in range(self.NUM_ROWS):
+                col.append(self.EMPTY)
             self.board.append(col)
 
     def setStartingPositions(self):
-        self.setChessNotation('f', 8, BLACK)
-        self.setChessNotation('h', 8, BLACK)
-        self.setChessNotation('b', 8, BLACK)
-        self.setChessNotation('d', 8, BLACK)
-        self.setChessNotation('a', 7, BLACK)
-        self.setChessNotation('c', 7, BLACK)
-        self.setChessNotation('e', 7, BLACK)
-        self.setChessNotation('g', 7, BLACK)
-        self.setChessNotation('b', 6, BLACK)
-        self.setChessNotation('d', 6, BLACK)
-        self.setChessNotation('f', 6, BLACK)
-        self.setChessNotation('h', 6, BLACK)
-        self.setChessNotation('a', 3, RED)
-        self.setChessNotation('c', 3, RED)
-        self.setChessNotation('e', 3, RED)
-        self.setChessNotation('g', 3, RED)
-        self.setChessNotation('b', 2, RED)
-        self.setChessNotation('d', 2, RED)
-        self.setChessNotation('f', 2, RED)
-        self.setChessNotation('h', 2, RED)
-        self.setChessNotation('a', 1, RED)
-        self.setChessNotation('c', 1, RED)
-        self.setChessNotation('e', 1, RED)
-        self.setChessNotation('g', 1, RED)
+        self.setChessNotation('f', 8, self.BLACK)
+        self.setChessNotation('h', 8, self.BLACK)
+        self.setChessNotation('b', 8, self.BLACK)
+        self.setChessNotation('d', 8, self.BLACK)
+        self.setChessNotation('a', 7, self.BLACK)
+        self.setChessNotation('c', 7, self.BLACK)
+        self.setChessNotation('e', 7, self.BLACK)
+        self.setChessNotation('g', 7, self.BLACK)
+        self.setChessNotation('b', 6, self.BLACK)
+        self.setChessNotation('d', 6, self.BLACK)
+        self.setChessNotation('f', 6, self.BLACK)
+        self.setChessNotation('h', 6, self.BLACK)
+        self.setChessNotation('a', 3, self.RED)
+        self.setChessNotation('c', 3, self.RED)
+        self.setChessNotation('e', 3, self.RED)
+        self.setChessNotation('g', 3, self.RED)
+        self.setChessNotation('b', 2, self.RED)
+        self.setChessNotation('d', 2, self.RED)
+        self.setChessNotation('f', 2, self.RED)
+        self.setChessNotation('h', 2, self.RED)
+        self.setChessNotation('a', 1, self.RED)
+        self.setChessNotation('c', 1, self.RED)
+        self.setChessNotation('e', 1, self.RED)
+        self.setChessNotation('g', 1, self.RED)
 
     def resetBoard(self):
         self.clearBoard()
         self.setStartingPositions()
 
     def printBoard(self):
-        for row in range(NUM_ROWS):
-            row = NUM_ROWS - row - 1
-            for col in range(NUM_COLUMNS):
+        for row in range(self.NUM_ROWS):
+            row = self.NUM_ROWS - row - 1
+            for col in range(self.NUM_COLUMNS):
                 print(self.get(col, row), end='')
             print()
 
     def isInBounds(self, col, row):
-        return col < NUM_COLUMNS and col >= 0 and row < NUM_ROWS and row >= 0
+        return col < self.NUM_COLUMNS and col >= 0 \
+                and row < self.NUM_ROWS and row >= 0
 
     def isInBoundsChessNotation(self, col, row):
         col = self.conv(col)
@@ -108,24 +110,24 @@ class Board:
         return self.getAdjacent(col, row, direction)
 
     def getOppositeColor(self, colorToSwap):
-        if colorToSwap is RED:
-            return BLACK
-        elif colorToSwap is BLACK:
-            return RED
+        if colorToSwap is self.RED:
+            return self.BLACK
+        elif colorToSwap is self.BLACK:
+            return self.RED
         else:
             raise ValueError("Bad color swap argument")
 
     def getOppositeColorKing(self, colorToSwap):
-        if colorToSwap is RED:
-            return BLACK_KING
-        elif colorToSwap is BLACK:
-            return RED_KING
+        if colorToSwap is self.RED:
+            return self.BLACK_KING
+        elif colorToSwap is self.BLACK:
+            return self.RED_KING
         else:
             raise ValueError("Bad king color swap argument")
 
     def getLegalMoves(self, col, row):
         piece = self.get(col, row)
-        if piece is EMPTY or piece is OOB:
+        if piece is self.EMPTY or piece is self.OOB:
             return []
 
         legalMoves = []
@@ -136,28 +138,28 @@ class Board:
         oppositeColor = self.getOppositeColor(piece)
         oppositeColorKing = self.getOppositeColorKing(piece)
 
-        if piece is RED or piece is RED_KING or piece is BLACK_KING:
-            if tl is EMPTY:
+        if piece is self.RED or piece is self.RED_KING or piece is self.BLACK_KING:
+            if tl is self.EMPTY:
                 legalMoves.append((col -1, row +1))
             elif tl is oppositeColor or tl is oppositeColorKing and \
-              self.getAdjacent(col -1, row +1, 'tl') is EMPTY:
+              self.getAdjacent(col -1, row +1, 'tl') is self.EMPTY:
                 legalMoves.append((col -1 -1, row +1 +1))
-            if tr is EMPTY:
+            if tr is self.EMPTY:
                 legalMoves.append((col +1, row +1))
             elif tr is oppositeColor or tr is oppositeColorKing and \
-               self.getAdjacent(col +1, row +1, 'tr') is EMPTY:
+               self.getAdjacent(col +1, row +1, 'tr') is self.EMPTY:
                 legalMoves.append((col +1 +1, row +1 +1))
 
-        if piece is BLACK or piece is RED_KING or piece is BLACK_KING:
-            if bl is EMPTY:
+        if piece is self.BLACK or piece is self.RED_KING or piece is self.BLACK_KING:
+            if bl is self.EMPTY:
                 legalMoves.append((col -1, row -1))
             elif bl is oppositeColor or bl is oppositeColorKing and \
-              self.getAdjacent(col -1, row -1, 'bl') is EMPTY:
+              self.getAdjacent(col -1, row -1, 'bl') is self.EMPTY:
                 legalMoves.append((col -1 -1, row -1 -1))
-            if br is EMPTY:
+            if br is self.EMPTY:
                 legalMoves.append((col +1, row -1))
             elif br is oppositeColor or br is oppositeColorKing and \
-               self.getAdjacent(col +1, row -1, 'br') is EMPTY:
+               self.getAdjacent(col +1, row -1, 'br') is self.EMPTY:
                 legalMoves.append((col +1 +1, row -1 -1))
 
         return legalMoves
