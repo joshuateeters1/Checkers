315e49cfc9bfd218f8e96b26e5de2cb2a80b8378
joshuateeters1 <joshuateeters1@gmail.com>
2022-02-13 17:41:43 -0600

    Now chdir to path main file is in

--- checkers.py
+++ checkers.py
@@ -1,12 +1,14 @@
 import sys
-sys.path.append(".")
+import os
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 from board import Board
 from visualization import Visual
 
 board = Board()
 visual = Visual()
 
+print(os.getcwd())
 #print the board
 board.printBoard()
 
-visual.updateVisual(board)
+#visual.updateVisual(board)
